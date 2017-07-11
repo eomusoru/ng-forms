e32e44bcfd9a05f84bbcdf7a662e2655db89ed1b
Eduard <eduardomusoru@gmail.com>
2017-07-11 10:56:51 +0300

    [practice/simple-reactive-form-with-async-valid] Another example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  statusValues = ['Stable', 'Critical', 'Finished'];
+  result: {};
+  formSubmited: boolean = false;
+  statusForm: FormGroup;
+
+  ngOnInit() {
+    this.statusForm = new FormGroup({
+      'name': new FormControl(null, Validators.required, this.validatorName.bind(this)),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('Stable')
+    });
+  }
+  
+  submitForm(){
+    this.formSubmited = true;
+    this.result = this.statusForm.value;
+  }
+
+  validatorName(control:FormControl): Promise<any> | Observable<any>{
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'Test'){
+          resolve({
+            'usernameIsForbidden': true
+          })
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+
+    return promise;
+  }
+  
 }
